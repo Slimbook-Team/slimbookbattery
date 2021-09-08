e6c81eb5afb7679063f7072080b0ec340f2752a8
slimbook <dev@slimbook.es>
2021-09-08 14:42:25 +0200

    Version 4.0 - Remake

--- src/service/slimbookbatteryservice.sh
+++ src/service/slimbookbatteryservice.sh
@@ -63,7 +63,7 @@ if [ -e /sys/class/power_supply/BAT0 ]; then
 	    #Sleeping to read if value changed
 		last_value=$CAPACITY
 		#echo "Last value: $last_value"
-	    	sleep 90
+	    	sleep 45
     done
 fi
 exit 0
