f9f08fab237e2391d320efb3496fe6f56900e9ce
Carlosmape <cagrmape@gmail.com>
2021-10-18 16:59:25 +0200

    Added dependency check process (just python ones by the moment). Added post-installation script

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,57 @@ echo "Press ENTER to proceed"
 echo "CTRL-C to exit"
 read
 
+#Used python binary
+python="python3"
+python_pck_installer="pip3"
+#Parse entire repository looking for python import string
+python_packages=$(grep -Re "^import" * | awk '{print $2}' | sort -u)
+
+echo "Checking Python dependencies..."
+echo -ne "Dependencies are: $python $python_pck_installer"
+python_packages=$python_packages
+echo $python_packages
+
+if [ ! $python ] && [ ! $python_pck_installer ];
+then
+	echo "Please install $python and $python_pck_installer"
+	exit 1
+else 
+	for package in $python_packages; do
+		if ! $($python -c "import $package" &> /dev/null);
+		then
+			echo "Installing \"$python_pck_installer $package\"..."
+			if $python_pck_installer install $package; 
+			then
+				echo "... done"
+			else
+				echo "... ERROR: could not install $package. Try to install manually and run this script again"
+				exit 1
+			fi
+		fi
+	done  
+fi
+
+echo
+echo "Check system dependencies..."
+echo "TODO :')'"
+
 echo "Removing previously installed resources..."
 sudo rm -rf /usr/share/slimbookbattery/*
 sudo rm /usr/bin/slimbookbattery /usr/bin/slimbookbattery-pkexec
 
+echo
 echo "Deploying..."
 sudo mkdir /usr/share/slimbookbattery
 sudo cp -vRf . /usr/share/slimbookbattery/
 
+echo
+echo "Creating binary simlinks..."
 sudo cp -s /usr/share/slimbookbattery/bin/* /usr/bin/
+
+echo "Checking installation (post installation script)"
+if ./src/check_config.py; then
+	echo "Done!"
+else
+	echo "ERROR: See below otput, could not check your installation porperly"
+fi
