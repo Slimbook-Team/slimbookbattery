Slimbook-Team/slimbookbattery

commit fe78b54ff66c0b2a8d0663e3ad26f20f28c8c4ec

Refactory of the code and fix to readme (#103)
* Change all the string interpolation with f string

* Simplified logical expression

* convert loops with list/generator comprehension

* converted file opening with with and simple file reading with pathlib

* remove unnecessary cast and converted missed file operation with with

* converted if to named expression

* Fixed eng spell errors

* Updated italian translation

* Reformatted python code with black

---------

Co-authored-by: Domefemia <domefemia@hotmail.com>

src/utils.py
@@ -9,84 +9,84 @@ import logging
 
 from pkg_resources import parse_version as parse_version
 
+
 def get_logger(logger_name, create_file=False):
 
-        log = logging.getLogger(logger_name)
-        log.setLevel(level=logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s:%(lineno)d - %(levelname)s - %(message)s')
+    log = logging.getLogger(logger_name)
+    log.setLevel(level=logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(funcName)s:%(lineno)d - %(levelname)s - %(message)s"
+    )
 
-        if create_file:
-            try:            
-                fh = logging.FileHandler('/var/log/slimbookbattery.log')
-            except PermissionError:
-                log.critical(
-                    'Cannot open log file /var/slimbookbattery.log, using /tmp/slimbookbattery.log')
-                fh = logging.FileHandler('/tmp/slimbookbattery.log')
-            if fh:
-                fh.setLevel(level=logging.ERROR)
-                fh.setFormatter(formatter)
+    if create_file:
+        try:
+            fh = logging.FileHandler("/var/log/slimbookbattery.log")
+        except PermissionError:
+            log.critical(
+                "Cannot open log file /var/slimbookbattery.log, using /tmp/slimbookbattery.log"
+            )
+            fh = logging.FileHandler("/tmp/slimbookbattery.log")
+        if fh:
+            fh.setLevel(level=logging.ERROR)
+            fh.setFormatter(formatter)
 
-            log.addHandler(fh)
+        log.addHandler(fh)
 
-        # console handler
-        ch = logging.StreamHandler()
-        ch.setLevel(level=logging.ERROR)
-        ch.setFormatter(formatter)
-        log.addHandler(ch)
-        return log 
+    # console handler
+    ch = logging.StreamHandler()
+    ch.setLevel(level=logging.ERROR)
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+    return log
+
 
 logger = get_logger("utils.py", True)
 
 PKG_MANAGERS = {
-        "rpm": {
-            "distros": [
-                "fedora",
-                "rhel",
-            ],
-            "install_command": "-i",
-            "get_app_list_cmd": "-qa",
-        },
-        "zypper": {
-            "distros": [
-                "suse",
-                "opensuse",
-            ],
-            "install_command": "in",
-            "get_app_list_cmd": None,
-        },
-        "apt": {
-            "distros": [
-                "ubuntu",
-                "debian",
-                "elementary"
-            ],
-            "install_command": "install",
-            "get_app_list_cmd": "list --installed",
-        },
-        "yum": {
-            "distros": [
-                "centos"
-            ],
-            "install_command": "install",
-            "get_app_list_cmd": None,
-        },
-        "pacman": {
-            "distros": [
-                "manjaro",
-                "arch",
-            ],
-            "install_command": "-S",
-            "get_app_list_cmd": "-Q",
-        }
-    }
+    "rpm": {
+        "distros": [
+            "fedora",
+            "rhel",
+        ],
+        "install_command": "-i",
+        "get_app_list_cmd": "-qa",
+    },
+    "zypper": {
+        "distros": [
+            "suse",
+            "opensuse",
+        ],
+        "install_command": "in",
+        "get_app_list_cmd": None,
+    },
+    "apt": {
+        "distros": ["ubuntu", "debian", "elementary"],
+        "install_command": "install",
+        "get_app_list_cmd": "list --installed",
+    },
+    "yum": {
+        "distros": ["centos"],
+        "install_command": "install",
+        "get_app_list_cmd": None,
+    },
+    "pacman": {
+        "distros": [
+            "manjaro",
+            "arch",
+        ],
+        "install_command": "-S",
+        "get_app_list_cmd": "-Q",
+    },
+}
 
 PACKAGES = {
-        "linux-tools-$(uname -r)": {
-            "rpm": "kernel-tools",
-            "pacman": "linux-tools-meta",
-            "apt": "linux-tools-$(uname -r)",
-        },
-    }
+    "linux-tools-$(uname -r)": {
+        "rpm": "kernel-tools",
+        "pacman": "linux-tools-meta",
+        "apt": "linux-tools-$(uname -r)",
+    },
+}
+
 
 def get_user(from_file=None):
     try:
@@ -96,47 +96,52 @@ def get_user(from_file=None):
 
     if from_file and os.path.exists(from_file):
         exit_code, candidate = subprocess.getstatusoutput(
-            'cat {} | tail -n 1 | cut -f 2 -d "@"'.format(from_file))
+            f'cat {from_file} | tail -n 1 | cut -f 2 -d "@"'
+        )
         if exit_code != 0:
             user_name = candidate
 
-    if user_name == 'root':
-        if 'SUDO_USER' in os.environ and os.environ['SUDO_USER'] != 'root':
-            user_name = os.environ['SUDO_USER']
+    if user_name == "root":
+        if "SUDO_USER" in os.environ and os.environ["SUDO_USER"] != "root":
+            user_name = os.environ["SUDO_USER"]
         else:
             var = 10
             while var > 0:
-                var = var - 1
+                var -= 1
                 user_name = subprocess.getoutput(
-                    'last -wn1 | head -n 1 | cut -f 1 -d " "')
-                logger.debug("Username: {}".format(user_name))
-                if user_name != 'reboot':
+                    'last -wn1 | head -n 1 | cut -f 1 -d " "'
+                )
+                logger.debug(f"Username: {user_name}")
+                if user_name != "reboot":
                     break
     return user_name
 
+
 # Returns pkg_man, install_cmd, and package name if it is provided as an argument
 
+
 def get_install_command(package_manager=None):
-    pkg_man = package_manager if package_manager else get_package_manager()
-    if pkg_man:
+    if pkg_man := package_manager or get_package_manager():
         try:
-            command = PKG_MANAGERS[pkg_man]['install_command']
+            command = PKG_MANAGERS[pkg_man]["install_command"]
             return command
         except Exception as e:
             logger.error("No distro install command found")
     return None
+
 
 def get_package_manager():
     id, id_like = get_os_info()
     pkg_man = None
     if id:
         for pkg_man in PKG_MANAGERS:
-            for distro in PKG_MANAGERS[pkg_man]['distros']:
+            for distro in PKG_MANAGERS[pkg_man]["distros"]:
                 if distro == id:
                     return pkg_man
     else:
-        logger.error('No distro id found')
+        logger.error("No distro id found")
     return None
+
 
 def get_package_name(apt_package_name):
     PACKAGES = {
@@ -147,7 +152,7 @@ def get_package_name(apt_package_name):
         },
     }
     pkg_man = get_package_manager()
-    
+
     for pkg_name in PACKAGES:
         if apt_package_name == pkg_name:
             for pkg_manager in PACKAGES[pkg_name]:
@@ -155,26 +160,30 @@ def get_package_name(apt_package_name):
                     logger.info(PACKAGES[pkg_name][pkg_manager])
                     return PACKAGES[pkg_name][pkg_manager]
 
-    logger.error(
-        "No package-name found for {}, in {}".format(apt_package_name, pkg_man))
+    logger.error(f"No package-name found for {apt_package_name}, in {pkg_man}")
     return None
 
+
 def check_package_installed(package_name):
-    pkg_name = get_package_name(package_name)
-    if pkg_name:
-        pkg_man = get_package_manager()
-        if pkg_man:
+    if pkg_name := get_package_name(package_name):
+        if pkg_man := get_package_manager():
             try:
-                cmd = PKG_MANAGERS[pkg_man]['get_app_list_cmd']
-                if subprocess.getstatusoutput("{} {} | grep {}".format(pkg_man, cmd, pkg_name))[0] == 0:
+                cmd = PKG_MANAGERS[pkg_man]["get_app_list_cmd"]
+                if (
+                    subprocess.getstatusoutput(f"{pkg_man} {cmd} | grep {pkg_name}")[0]
+                    == 0
+                ):
                     return True
             except Exception:
                 print(Exception)
                 logger.error("Could not get list of installed apps.")
     return False
+
+
 def get_os_info():
-    subprocess.getstatusoutput('cat /etc/*-release')
+    subprocess.getstatusoutput("cat /etc/*-release")
     import csv
+
     OS_INFO = {}
     with open("/etc/os-release") as f:
         reader = csv.reader(f, delimiter="=")
@@ -182,19 +191,16 @@ def get_os_info():
             if row:
                 OS_INFO[row[0]] = row[1]
 
-    if OS_INFO['ID']:
-        id = OS_INFO['ID']
-        if OS_INFO['ID_LIKE']:
-            id_like = OS_INFO['ID_LIKE']
-        else:
-            id_like = None
+    if OS_INFO["ID"]:
+        id = OS_INFO["ID"]
+        id_like = OS_INFO["ID_LIKE"] or None
     else:
         id = None
     return id, id_like
 
 
 def get_languages():
-    languages = ['en']
+    languages = ["en"]
     try:
         user_environ = locale.getlocale()[0]
         for lang in ["en", "es", "it", "pt", "gl"]:
@@ -210,54 +216,57 @@ def get_tlp_conf_file():
 
     code, res = subprocess.getstatusoutput('tlp-stat --config | grep "TLP "')
     if code == 0:
-        res = res[res.find('TLP'):-1]
-        version = res.split(' ')[1]
+        res = res[res.find("TLP") : -1]
+        version = res.split(" ")[1]
     else:
-        version = '1.3'  # Most common
+        version = "1.3"  # Most common
     try:
-        if parse_version(version) >= parse_version('1.3'):
-            file = '/etc/tlp.conf'
+        if parse_version(version) >= parse_version("1.3"):
+            file = "/etc/tlp.conf"
         else:
-            file = '/etc/default/tlp'
+            file = "/etc/default/tlp"
     except Exception as ex:
-        print(str(ex)+'\nTLP version not found, using TLP 1.3 config file.')
-        file = '/etc/tlp.conf'
+        print(str(ex) + "\nTLP version not found, using TLP 1.3 config file.")
+        file = "/etc/tlp.conf"
     return file, version
+
 
 def get_version(v):
     return parse_version(v)
+
 
 def load_translation(filename):
     current_path = os.path.dirname(os.path.realpath(__file__))
     languages = get_languages()
     return gettext.translation(
         filename,
-        os.path.join(current_path, 'translations'),
+        os.path.join(current_path, "translations"),
         languages=languages,
-        fallback=True
+        fallback=True,
     ).gettext
+
 
 def get_display_resolution():
     dimensions = subprocess.getoutput("xdpyinfo | grep 'dimensions:'")
     dimensions = dimensions.split()
     dimensions = dimensions[1]
-    dimensions = dimensions.split('x')
+    dimensions = dimensions.split("x")
 
     return dimensions
 
+
 def reboot_process(process_name, path):
-    process = subprocess.getoutput('pgrep -f ' + process_name)
+    process = subprocess.getoutput(f"{pkg_man} {cmd} | grep {pkg_name}")
     # If it finds a process, kills it
-    if len(process.split('\n')) > 0:
-        proc_list = process.split('\n')
-        print('Processes: ', proc_list)
+    if len(process.split("\n")) > 0:
+        proc_list = process.split("\n")
+        print("Processes: ", proc_list)
         for i in range(len(proc_list)):
             try:
-                subprocess.getstatusoutput('kill -9 ' + proc_list[i])
+                subprocess.getstatusoutput(f"kill -9 {proc_list[i]}")
             except:
-                print('Killing process failed')
-    if os.path.isfile(path):
-        if os.system('python3 {} &'.format(path)) == 0:
-            return (0, 'Process killed & launched')
-    else:
-        return (1, 'Process launch path does not exist')
+                print("Killing process failed")
+    if not os.path.isfile(path):
+        return (1, "Process launch path does not exist")
+    if os.system(f"python3 {path} &") == 0:
+        return (0, "Process killed & launched")

src/ac_power.py
@@ -14,27 +14,25 @@ if CURRENT_PATH not in sys.path:
 import utils
 
 USER_NAME = utils.get_user()
-HOMEDIR = os.path.expanduser('~{}'.format(USER_NAME))
+HOMEDIR = os.path.expanduser(f"~{USER_NAME}")
 
-_ = utils.load_translation('preferences')
+_ = utils.load_translation("preferences")
 
-config_file = HOMEDIR + '/.config/slimbookbattery/slimbookbattery.conf'
+config_file = f"{HOMEDIR}/.config/slimbookbattery/slimbookbattery.conf"
 
 config = configparser.ConfigParser()
 config.read(config_file)
 
-cmd = (subprocess.getstatusoutput("cat /sys/class/power_supply/BAT0/status"))
+cmd = subprocess.getstatusoutput("cat /sys/class/power_supply/BAT0/status")
 
 if cmd[0] == 0:
     status = cmd[1]
 
-    if status == 'Discharging':
-        config.set('CONFIGURATION', 'plugged', str(date.today()))
-        configfile = open(config_file, 'w')
-        config.write(configfile)
-        configfile.close()
-
+    if status == "Discharging":
+        config.set("CONFIGURATION", "plugged", str(date.today()))
+        with open(config_file, "w") as configfile:
+            config.write(configfile)
 else:
-    status = (_('Unknown'))
+    status = _("Unknown")
 
 print(status)

src/check_config.py
@@ -17,14 +17,14 @@ import utils
 
 USER_NAME = utils.get_user()
 
-HOMEDIR = os.path.expanduser('~{}'.format(USER_NAME))
+HOMEDIR = os.path.expanduser(f"~{USER_NAME}")
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
-DEFAULT_CONF = os.path.join(CURRENT_PATH,'configuration', 'slimbookbattery.conf')
-CONFIG_FOLDER = os.path.join(HOMEDIR, '.config/slimbookbattery')
-CONFIG_FILE = os.path.join(CONFIG_FOLDER, 'slimbookbattery.conf')
+DEFAULT_CONF = os.path.join(CURRENT_PATH, "configuration", "slimbookbattery.conf")
+CONFIG_FOLDER = os.path.join(HOMEDIR, ".config/slimbookbattery")
+CONFIG_FILE = os.path.join(CONFIG_FOLDER, "slimbookbattery.conf")
 
-UPDATES_DIR = os.path.join(CURRENT_PATH, 'updates')
+UPDATES_DIR = os.path.join(CURRENT_PATH, "updates")
 
 uid, gid = pwd.getpwnam(USER_NAME).pw_uid, pwd.getpwnam(USER_NAME).pw_gid
 
@@ -33,18 +33,20 @@ logger = logging.getLogger()
 
 def main():
     if not os.path.isdir(CONFIG_FOLDER):
-        logger.info('Creating config folder ...')
+        logger.info("Creating config folder ...")
         os.umask(0)
         os.makedirs(CONFIG_FOLDER, mode=0o766)  # creates with perms
         os.chown(CONFIG_FOLDER, uid, gid)  # set user:group
-        logger.info(subprocess.getoutput('ls -la ' + CONFIG_FOLDER))
+        logger.info(subprocess.getoutput(f"ls -la {CONFIG_FOLDER}"))
     else:
-        logger.info('Configuration folder ({}) found!'.format(CONFIG_FOLDER))
+        logger.info(f"Configuration folder ({CONFIG_FOLDER}) found!")
 
     tlp_conf = utils.get_tlp_conf_file()[0]
     if not os.path.exists(tlp_conf):
-        shutil.copyfile(os.path.join(CURRENT_PATH, 'configuration', 'default', 'equilibrado'), tlp_conf)
-
+        shutil.copyfile(
+            os.path.join(CURRENT_PATH, "configuration", "default", "equilibrado"),
+            tlp_conf,
+        )
 
     set_ownership(CONFIG_FOLDER)
     set_ownership(UPDATES_DIR)
@@ -52,14 +54,15 @@ def main():
     check_config_file()
     check_tlp_files()
 
+
 def set_ownership(folder):
     folder_stat = os.stat(folder)
-    logger.debug("Folder {}\nUser uid={}\nFolder uid={}".format(folder, uid, folder_stat.st_uid))
+    logger.debug(f"Folder {folder}\nUser uid={uid}\nFolder uid={folder_stat.st_uid}")
     f_uid = folder_stat.st_uid
     f_gid = folder_stat.st_gid
 
-    if not uid == f_uid or not gid == f_gid:
-        #logger.info('Setting {} ownership').format(folder)
+    if uid != f_uid or gid != f_gid:
+        # logger.info('Setting {} ownership').format(folder)
 
         for dir_path, dir_name, filenames in os.walk(folder):
             logger.debug(dir_path)
@@ -69,8 +72,9 @@ def set_ownership(folder):
                 logger.debug(file_path)
                 os.chown(file_path, uid, gid)
 
+
 def check_config_file():
-    logger.info('Checking Slimbook Battery Configuration')
+    logger.info("Checking Slimbook Battery Configuration")
     if os.path.isfile(CONFIG_FILE):
         config = configparser.ConfigParser()
         config.read(CONFIG_FILE)
@@ -80,30 +84,30 @@ def check_config_file():
         incidences = False
 
         for section in default_config.sections():
-            logger.info('Checking section: {} ...'.format(section))
+            logger.info(f"Checking section: {section} ...")
             if not config.has_section(section):
                 incidences = True
                 config.add_section(section)
-                logger.info('Section added')
+                logger.info("Section added")
 
             for var in default_config.options(section):
                 if not config.has_option(section, var):
                     incidences = True
-                    logger.info('Not found: {}'.format(var))
+                    logger.info(f"Not found: {var}")
                     config.set(section, var, default_config.get(section, var))
 
         if incidences:
             try:
-                with open(CONFIG_FILE, 'w') as configfile:
+                with open(CONFIG_FILE, "w") as configfile:
                     config.write(configfile)
-                logger.info('Incidences corrected.')
+                logger.info("Incidences corrected.")
             except Exception:
-                logger.exception('Incidences could not be corrected.')
+                logger.exception("Incidences could not be corrected.")
         else:
-            logger.info('Incidences not found.')
+            logger.info("Incidences not found.")
 
     else:
-        logger.info('Creating config file ...')
+        logger.info("Creating config file ...")
         shutil.copy(DEFAULT_CONF, CONFIG_FOLDER)
         os.chown(CONFIG_FILE, uid, gid)  # set user:group
 
@@ -111,17 +115,15 @@ def check_config_file():
 # Checks if the user's default config files exist an if they are like the app version's default files.
 # If files or directories don't exist they are created.
 def check_tlp_files():
-    
-    
-    
+
     logger.info("Checking Slimbook Battery's TLP Configuration")
 
     incidences = False
-    usr_custom_dir = os.path.join(CONFIG_FOLDER, 'custom/')
-    usr_default_dir = os.path.join(CONFIG_FOLDER, 'default/')
-    app_default_dir = os.path.join(CURRENT_PATH, '../default/')
+    usr_custom_dir = os.path.join(CONFIG_FOLDER, "custom/")
+    usr_default_dir = os.path.join(CONFIG_FOLDER, "default/")
+    app_default_dir = os.path.join(CURRENT_PATH, "../default/")
 
-    files = ['ahorrodeenergia', 'equilibrado', 'maximorendimiento']
+    files = ["ahorrodeenergia", "equilibrado", "maximorendimiento"]
 
     # Checks if default files are the same in the app folder and in the system,
     # if something changed all will be restored
@@ -132,20 +134,25 @@ def check_tlp_files():
 
         # If user default is different from app default -- updates
         if os.path.isfile(usr_default_file) and os.path.isfile(app_default_file):
-            logger.info('Checking {}'.format(file))
-            if subprocess.getstatusoutput('diff {} {}'.format(app_default_dir, usr_default_dir))[0] != 0:
-                logger.info('Default files have changed, updating:')
+            logger.info(f"Checking {file}")
+            if (
+                subprocess.getstatusoutput(f"diff {app_default_dir} {usr_default_dir}")[
+                    0
+                ]
+                != 0
+            ):
+                logger.info("Default files have changed, updating:")
                 incidences = True
         else:
-            logger.info('{} does not exist.'.format(usr_default_file))
+            logger.info(f"{usr_default_file} does not exist.")
             incidences = True
 
     if incidences:
-        logger.info('Resetting default and custom files ...')
+        logger.info("Resetting default and custom files ...")
 
         if os.path.isdir(app_default_dir):
             if not (os.path.isdir(usr_default_dir) and os.path.isdir(usr_custom_dir)):
-                logger.info('Creating directories')
+                logger.info("Creating directories")
                 os.makedirs(usr_default_dir, mode=0o766)
                 os.chown(usr_default_dir, uid, gid)  # set user:group
                 os.mkdir(usr_custom_dir, mode=0o766)
@@ -155,21 +162,25 @@ def check_tlp_files():
                 app_default_file = os.path.join(app_default_dir, file)
                 shutil.copy(app_default_file, usr_default_dir)
                 shutil.copy(app_default_file, usr_custom_dir)
-                os.chown(os.path.join(usr_default_dir, file), uid, gid)  # set user:group
+                os.chown(
+                    os.path.join(usr_default_dir, file), uid, gid
+                )  # set user:group
                 os.chown(os.path.join(usr_custom_dir, file), uid, gid)  # set user:group
         else:
-            logger.error('Base default file not found')
+            logger.error("Base default file not found")
     else:
-        logger.info('Incidences not found.')
+        logger.info("Incidences not found.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(funcName)s:%(lineno)d - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(funcName)s:%(lineno)d - %(levelname)s - %(message)s"
+    )
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    logger.info('Config check executed as {}'.format(USER_NAME))
+    logger.info(f"Config check executed as {USER_NAME}")
     main()

commit c0e901cf33898b0240a4a99fc7414460f3843cf1

Fix tests (#108)
* Update test workflow

- Remove Python EOL versions. Tests currently fail because 3.6 does not
  exist in ubuntu-latest. 3.7 EOL is 27th June 2023.
- Add newer Python versions.
- Fix Python versions not specified as strings.
- Use newer version of actions.
- Add newlines between steps.

* src/utils.py: Fix undefined name error and fix formatting

Line 259 was changed by commit fe78b54ff66c to be something else
entirely, and have undefined names, which causes flake8 to report it.

Thus fix it.

Also do some formatting and style fixes.

Fixes: fe78b54ff66c ("Refactory of the code and fix to readme (#103)")

.github/workflows/test.yml
@@ -13,18 +13,21 @@ jobs:
     strategy:
       max-parallel: 4
       matrix:
-        python-version: [3.6, 3.7, 3.8, 3.9]
+        python-version: ['3.8', '3.9', '3.10', '3.11']
 
     steps:
-      - uses: actions/checkout@v2
+      - uses: actions/checkout@v3
+
       - name: Set up Python ${{ matrix.python-version }}
-        uses: actions/setup-python@v2
+        uses: actions/setup-python@v4
         with:
           python-version: ${{ matrix.python-version }}
+
       - name: Install Dependencies
         run: |
           python -m pip install --upgrade pip
           pip install flake8
+
       - name: Lint with flake8
         run: |
           # stop the build if there are Python syntax errors or undefined names

src/utils.py
@@ -1,8 +1,6 @@
 import getpass
 import gettext
 import os
-from re import sub
-import sys
 import subprocess
 import locale
 import logging
@@ -14,17 +12,16 @@ def get_logger(logger_name, create_file=False):
 
     log = logging.getLogger(logger_name)
     log.setLevel(level=logging.DEBUG)
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(funcName)s:%(lineno)d - %(levelname)s - %(message)s"
-    )
+    formatter = logging.Formatter("%(asctime)s - %(name)s - "
+                                  "%(funcName)s:%(lineno)d - "
+                                  "%(levelname)s - %(message)s")
 
     if create_file:
         try:
             fh = logging.FileHandler("/var/log/slimbookbattery.log")
         except PermissionError:
-            log.critical(
-                "Cannot open log file /var/slimbookbattery.log, using /tmp/slimbookbattery.log"
-            )
+            log.critical("Cannot open log file /var/slimbookbattery.log, "
+                         "using /tmp/slimbookbattery.log")
             fh = logging.FileHandler("/tmp/slimbookbattery.log")
         if fh:
             fh.setLevel(level=logging.ERROR)
@@ -51,6 +48,7 @@ PKG_MANAGERS = {
         "install_command": "-i",
         "get_app_list_cmd": "-qa",
     },
+
     "zypper": {
         "distros": [
             "suse",
@@ -59,16 +57,19 @@ PKG_MANAGERS = {
         "install_command": "in",
         "get_app_list_cmd": None,
     },
+
     "apt": {
         "distros": ["ubuntu", "debian", "elementary"],
         "install_command": "install",
         "get_app_list_cmd": "list --installed",
     },
+
     "yum": {
         "distros": ["centos"],
         "install_command": "install",
         "get_app_list_cmd": None,
     },
+
     "pacman": {
         "distros": [
             "manjaro",
@@ -117,7 +118,7 @@ def get_user(from_file=None):
     return user_name
 
 
-# Returns pkg_man, install_cmd, and package name if it is provided as an argument
+# Returns pkg_man, install_cmd, & package name if it is provided as an argument
 
 
 def get_install_command(package_manager=None):
@@ -125,7 +126,7 @@ def get_install_command(package_manager=None):
         try:
             command = PKG_MANAGERS[pkg_man]["install_command"]
             return command
-        except Exception as e:
+        except Exception:
             logger.error("No distro install command found")
     return None
 
@@ -170,8 +171,9 @@ def check_package_installed(package_name):
             try:
                 cmd = PKG_MANAGERS[pkg_man]["get_app_list_cmd"]
                 if (
-                    subprocess.getstatusoutput(f"{pkg_man} {cmd} | grep {pkg_name}")[0]
-                    == 0
+                    subprocess.getstatusoutput(
+                        f"{pkg_man} {cmd} | grep {pkg_name}"
+                    )[0] == 0
                 ):
                     return True
             except Exception:
@@ -216,7 +218,7 @@ def get_tlp_conf_file():
 
     code, res = subprocess.getstatusoutput('tlp-stat --config | grep "TLP "')
     if code == 0:
-        res = res[res.find("TLP") : -1]
+        res = res[res.find("TLP"):-1]
         version = res.split(" ")[1]
     else:
         version = "1.3"  # Most common
@@ -256,7 +258,7 @@ def get_display_resolution():
 
 
 def reboot_process(process_name, path):
-    process = subprocess.getoutput(f"{pkg_man} {cmd} | grep {pkg_name}")
+    process = subprocess.getoutput(f"pgrep -f {process_name}")
     # If it finds a process, kills it
     if len(process.split("\n")) > 0:
         proc_list = process.split("\n")
@@ -264,7 +266,7 @@ def reboot_process(process_name, path):
         for i in range(len(proc_list)):
             try:
                 subprocess.getstatusoutput(f"kill -9 {proc_list[i]}")
-            except:
+            except Exception:
                 print("Killing process failed")
     if not os.path.isfile(path):
         return (1, "Process launch path does not exist")